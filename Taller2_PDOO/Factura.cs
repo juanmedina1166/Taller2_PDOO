1f0c81134302388746f6a91b1600ebaa099052a5
Nowohj <158013672+Nowohj@users.noreply.github.com>
2024-04-08 00:29:46 -0500

    Adicion de clase factura

--- Taller2_PDOO/Factura.cs
+++ Taller2_PDOO/Factura.cs
@@ -8,5 +8,42 @@ namespace Taller2_PDOO
 {
     internal class Factura
     {
+        private string fecha;
+        private string estado_actual;
+        private string estado_pago:
+        private float iva;
+        private float total;
+        private Producto[] canasta;
+        private int indice;
+        private int numero_factura;
+
+        public Factura(){
+
+        }
+
+        public Factura(int cantidad_productos)
+        {
+            canasta = new Producto[cantidad_productos];
+        }
+
+        public Factura(string fecha, int estado_actual, string medio_pago, float )
+            this.fecha = fecha;
+            this.estado_actual = estado_actual;
+            this.medio_pago = medio_pago;
+            this.iva = iva;
+            this.total = total;
+
+    }
+
+    public void MostrarProducto(int indice)
+    {
+        int cantidiad = canasta[indice].Cantidad;
+        Console.Write(Utilitario.FormatearDigito(cantidad.ToString()));
+        Utilitario
+        Console.Wrie(Utilitario);
+        Utilitario.ImprimirEspacios(1);
+        Console.WriteLine(Utilitario.ImprimirEspaciosInicio(canasta[indice].Pro))
+        Utilitario.ImprimirEspacios(1);
+        Console.WriteLine(utilitario.ImprimirEspaciosInicio((canasta[indice]))
     }
 }
